8137185f096b493df53d7776e1c1663832352dc7
Mischief <119103845+Mischief-Coding@users.noreply.github.com>
2024-10-17 21:47:42 +0100

    added all styling and validation check (need to add post request to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,62 @@
+"use client";
 import Image from "next/image";
-
+import styles from "./Home.module.css";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  function add_shark() {
+    //alert("api link: https://x8ki-letl-twmt.n7.xano.io/api:2D0WNQvF/shark_data");
+    console.log("work1")
+    const formInputs = document.querySelectorAll<HTMLInputElement>(`.${styles.textInput}`);
+    const unmetRequirement = document.querySelector<HTMLParagraphElement>(`.${styles.requirementsNotMet}`);
+    let unfinished = false;
+    formInputs.forEach((formInput) => {
+      if (formInput.required === true && (formInput.value === null || formInput.value === " " || formInput.value === "")  ) {
+        formInput.required === true ? formInput.classList.add(`${styles.requirementFailed}`) : ""; 
+        unfinished = true;
+
+      }
+      console.log(formInput.value)
+    }) 
+
+    unfinished ? unmetRequirement?.classList.add(`${styles.unmet}`) : "";
+  }
+  return (
+    <main>
+      <div className={styles.titleContainer}>
+        <p className={styles.title}>Miyaru Shark Report</p>
+      </div>
+      <div className={styles.form}>
+        <div className={`${styles.input} ${styles.inputText}`}>
+          <p className={styles.inputHeader}>Shark Name:</p>
+          <input className={styles.textInput} placeholder="Enter the shark name here..." id="sharkname" required={true}></input>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className={`${styles.input} ${styles.inputText}`}>
+          <p className={styles.inputHeader}>Shark Description:</p>
+          <input className={styles.textInput} placeholder="Enter the shark name here..." id="sharkdescription" required={true}></input>
+        </div>
+        <div className={`${styles.input} ${styles.inputText}`}>
+          <p className={styles.inputHeader}>GPS Location:</p>
+          <input className={styles.textInput} placeholder="Enter the shark name here..." id="gps_location" required={true}></input>
+        </div>
+        <div className={`${styles.input} ${styles.inputText}`}>
+          <p className={styles.inputHeader}>Approximate size:</p>
+          <input className={styles.textInput} placeholder="Enter the shark name here..." id="approximate_size" required={true}></input>
+        </div>
+        <div className={`${styles.input} ${styles.inputText}`}>
+          <p className={styles.inputHeader}>Features of surrounding area:</p>
+          <input className={styles.textInput} placeholder="Enter the shark name here..." id="surroundfeatures"></input>
+        </div>
+
+        <input type="submit" className={styles.formSubmit} name="Submit Report" onClick={add_shark}></input>
+
+        <p className={styles.requirementsNotMet}>Please enter all the requied information.</p>
+      </div>
+
+      <div className={styles.media}>
+        <img className={styles.sharkImage} src="https://lh7-us.googleusercontent.com/cQgCpYQLeh5uE32coK0lbMIlkxVz3cLFyq-9x_8d_OSI5q5sLPn_BT-4ZqeeYTAkAUDhdYjTMNwFSfZhXIDIbVkUJkO6WL3Mt_8FOVsajUNRZseheOoaHMhSb3lRcXEJ_rZjHZ1UTZhwMpcQEjYneecgh8N3Jg" alt="shark image"></img>
+      </div>
+
+
+    </main>
   );
 }
